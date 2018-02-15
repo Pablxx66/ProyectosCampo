01fd886808d4e2d8fd6da46a8a00818c2a54c1c7
Pachu <phax,rod@gmail.com>
2018-02-15 16:10:32 -0400

    La estructura basica casi definitiva

--- app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/ActivityPrincipal.java
+++ app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/ActivityPrincipal.java
@@ -1,7 +1,9 @@
-package com.llokallasoftware.proyectoscampo;
+package com.llokallasoftware.proyectoscampo.Vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.llokallasoftware.proyectoscampo.R;
 
 public class ActivityPrincipal extends AppCompatActivity {
 
---
+++ app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/Fragmentos/DatosCalculo.java
@@ -0,0 +1,30 @@
+package com.llokallasoftware.proyectoscampo.Vista.Fragmentos;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.llokallasoftware.proyectoscampo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatosCalculo extends Fragment {
+
+
+    public DatosCalculo() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragdatcalc, container, false);
+    }
+
+}
