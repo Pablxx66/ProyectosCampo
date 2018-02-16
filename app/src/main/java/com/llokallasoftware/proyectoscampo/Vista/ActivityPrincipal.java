4da4a484cc1f3984a172eccb4554ae8f4a875c82
Pachu <phax,rod@gmail.com>
2018-02-16 00:10:53 -0400

    Segundo commit con interaccion de fragmentos

--- app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/ActivityPrincipal.java
+++ app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/ActivityPrincipal.java
@@ -1,15 +1,84 @@
 package com.llokallasoftware.proyectoscampo.Vista;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.llokallasoftware.proyectoscampo.R;
+import com.llokallasoftware.proyectoscampo.Vista.Fragmentos.DatosCalculo;
+import com.llokallasoftware.proyectoscampo.Vista.Fragmentos.DatosProyecto;
 
 public class ActivityPrincipal extends AppCompatActivity {
+
+    Fragment fragProy = new DatosProyecto();
+    Fragment fragCalc = new DatosCalculo();
+
+    FragmentManager fragMan;
+    FragmentTransaction fragTrans;
+
+
+    public String[] mApps = {
+            "Instagram",
+            "Pinterest",
+            "Pocket",
+            "Twitter"
+    };
+
+
+    Spinner listaProyectos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        fragMan = getSupportFragmentManager();
+        fragTrans = fragMan.beginTransaction();
+        fragTrans.add(R.id.layRaiz, fragProy, "FragA");
+        fragTrans.commit();
+
         setContentView(R.layout.activity_principal);
+
+
     }
+
+
+    //dummy void
+    public void siguienteActividad(View v){
+
+        //Toast.makeText(this,"Cargando datos...",Toast.LENGTH_LONG).show();
+
+        FragmentTransaction transaction = fragMan.beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+        transaction.replace(R.id.layRaiz, fragCalc);
+        transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
+  }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        listaProyectos = fragMan.findFragmentByTag("FragA").getView().findViewById(R.id.spListaProy);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, mApps);
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        listaProyectos.setAdapter(adaptador);
+    }
+
+
 }
--- app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/Fragmentos/DatosProyecto.java
+++ app/src/main/java/com/llokallasoftware/proyectoscampo/Vista/Fragmentos/DatosProyecto.java
@@ -2,13 +2,16 @@ package com.llokallasoftware.proyectoscampo.Vista.Fragmentos;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.llokallasoftware.proyectoscampo.R;
 
@@ -20,39 +23,17 @@ import java.util.List;
  */
 public class DatosProyecto extends Fragment {
 
-    List<String> spinnerArray =  new ArrayList<String>();
-
-    public String[] mApps = {
-            "Instagram",
-            "Pinterest",
-            "Pocket",
-            "Twitter"
-    };
-
-    Spinner listaProyectos;
-
-
 
     public DatosProyecto() {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment // Required empty public constructor
-        spinnerArray.add("dodo");
-        spinnerArray.add("cucu");
-        spinnerArray.add("nene");
 
-       // listaProyectos = getView().findViewById(R.id.spListaProy);
-
-       // ArrayAdapter<String> adaptador = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, mApps);
-
-
-        //listaProyectos.setAdapter(adaptador);
         return inflater.inflate(R.layout.fragdatosproy, container, false);
     }
+
 
 }
